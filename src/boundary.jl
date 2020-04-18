9cb2a9395cadd4fafaaf97d63664103e92cf6f4e
Stephan Hilb <stephan@ecshi.net>
2020-04-18 14:43:30 +0200

    add some documentation

--- src/boundary.jl
+++ src/boundary.jl
@@ -1,3 +1,11 @@
+"""
+    getindex_boundary(w::Window, wi::CartesianCoordinate, b::Boundary)
+
+Return a value for an indexing operation on `w` with relative index `wi`
+respecting the boundary behaviour `b`.
+Users may define their own boundary behaviour by defining additional methods
+for this function.
+"""
 @inline getindex_boundary(_, _, b::Boundary) =
     throw(ArgumentError("undefined boundary behaviour for boundary type $b"))
 
@@ -19,6 +27,6 @@ end
 
 @inline function getindex_boundary(w, wi, b::BoundarySymmetric)
     pi = position(w) - wi
-    pimod = CartesianIndex(map(mod1, Tuple(pi), w.parent_size))
+    pimod = CartesianIndex(mod1.(Tuple(pi), w.parent_size))
     return getindex_parent(w, pimod)
 end
