3a3919c7d46edef6fa443ef3402ec247cb28eb63
Stephan Hilb <stephan@ecshi.net>
2020-04-21 17:16:48 +0200

    add tests for `setindex!` and `Base.map` performance

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,29 +5,11 @@ using StaticKernels: Window
 
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 0.1
 
-a = rand(rand(5:10, 3)...)
-
-dones = ntuple(i -> 1, ndims(a))
-dzeros = ntuple(i -> 0, ndims(a))
 
 @testset "consistency" begin
-    @testset for i in 1:ndims(a)
-        ddir = ntuple(j -> Int(j == i), ndims(a))
-
-        k = Kernel{UnitRange.(dzeros, ddir)}() do w
-            return something(w[ddir...], 0) - w[dzeros...]
-        end
-
-        @test isbits(k)
-
-        x = map(k, a)
-        y = diff(a, dims=i)
-
-        @test x[axes(k, a)...] ≈ y
-    end
+    a = rand(10, 10)
 
     @testset "gradient" begin
-        a = rand(10, 10)
         grad = Kernel{(0:1,0:1)}(w -> (w[1,0] - w[0,0], w[0,1] - w[0,0]))
 
         grada = map(grad, a)
@@ -36,12 +18,19 @@ dzeros = ntuple(i -> 0, ndims(a))
         @test diff(a, dims=2)[gx...] ≈ [x[2] for x in grada[gx...]]
     end
 
-    @testset "window as array" begin
-        a = rand(3, 3)
+    @testset "window" begin
+        b = similar(a)
+        c = CartesianIndex(2, 2)
         k = Kernel{(-1:1, -1:1)}(identity)
-        w = Window{(-1:1, -1:1)}(k, a, CartesianIndex(2, 2))
+        w = Window{axes(k)}(k, a, c)
 
-        @test sum(Tuple(w)) ≈ sum(a)
+        # getindex / setindex
+        for i in CartesianIndices(axes(k))
+            @inferred w[i]
+            @test w[i] == a[c + i]
+            w[i] = b[c + i]
+            @test a[c + i] == w[i]
+        end
     end
 end
 
@@ -55,15 +44,11 @@ end
         @test isconcretetype(eltype(k, a))
     end
 
-    @testset "window scalar getindex" begin
+    @testset "kernel evaluation" begin
         k = Kernel{(0:0,)}(w -> w[0])
-
         @test eltype(k, a) == Float64
-    end
 
-    @testset "window tuple" begin
         k = Kernel{(0:0,)}(w -> Tuple(w))
-
         @test eltype(k, a) <: Tuple
         @test isconcretetype(eltype(k, a))
     end
@@ -84,11 +69,19 @@ end
 end
 
 @testset "performance" begin
+    a = rand(1000000)
+
     @testset "Base.diff" begin
-        a = rand(10000)
         k = Kernel{(0:1,)}((@inline function(w) @inbounds w[1] - w[0] end))
 
         @test 1.3 > @belapsed(map($k, $a)) / @belapsed(diff($a))
+    end
+
+    @testset "Base.map" begin
+        k = Kernel{(0:0,)}((@inline function(w) @inbounds w[0] end))
+        b = similar(a, size(k, a))
+
+        @test 1.1 > @belapsed(map!($k, $b, $a)) / @belapsed(map!(identity, $b, $a))
     end
 end
 
