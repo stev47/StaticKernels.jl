9b0c942262d0cd64f74de2c25be3943582313f31
Stephan Hilb <stephan@ecshi.net>
2020-04-21 21:52:56 +0200

    add extension benchmark

--- test/benchmark_regression.jl
+++ test/benchmark_regression.jl
@@ -50,3 +50,10 @@ function laplace(n)
 end
 println("laplace")
 foreach(laplace, (1000, 3000))
+
+a = rand(1000, 1000)
+@inline kf(w) = @inbounds w[0,-1] + w[-1,0] - 4*w[0,0] + w[1,0] + w[0,1]
+k = Kernel{(-1:1,-1:1)}(kf, StaticKernels.ExtensionConstant(0))
+b = similar(a, size(k, a))
+
+@mybench map!($k, $b, $a)
