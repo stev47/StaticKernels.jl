4fbb1e1308181580bd50792ed551933466c060b5
Stephan Hilb <stephan@ecshi.net>
2020-04-26 11:18:27 +0200

    restructure benchmark

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -9,73 +9,76 @@ import LocalFilters
 using OffsetArrays: OffsetArray
 using LoopVectorization: @avx
 
-# from discourse forum:
-# https://discourse.julialang.org/t/ann-statickernels-jl-fast-kernel-operations-on-arrays/37658
-function filter2davx!(out::AbstractMatrix, A::AbstractMatrix, kern)
-    @avx for J in CartesianIndices(out)
-        tmp = zero(eltype(out))
-        for I ∈ CartesianIndices(kern)
-            tmp += A[I + J] * kern[I]
-        end
-        out[J] = tmp
-    end
-    out
-end
-
-# Linear Filtering
-
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 0.5
 pd = 19
 
-for N in (10, 100, 1000), K in (3, 5, 7)
-    a = rand(N, N)
-    k = rand(K, K)
+kernel_axes(k) = map(n -> -n÷2:n÷2, size(k))
+inner_axes(k, a) = map(axes(a), kernel_axes(k)) do ax, kx
+        first(ax) - first(kx) : last(ax) - last(kx)
+    end
+inner_size(k, a) = length.(inner_axes(k, a))
 
-    println("Array: $(size(a)), Kernel: $(size(k))")
-
-
-    # StaticKernels.jl
+function bench_linfilt(k, a, ::Val{:StaticKernels})
     print(rpad("  StaticKernels", pd))
 
     ktuple = Tuple(k)
     @inline function wf(w)
-        return sum(SVector(Tuple(w) .* ktuple))
+        return sum(SVector(Tuple(w)) .* SVector(ktuple))
     end
-    kern = Kernel{(-K÷2:K÷2, -K÷2:K÷2)}(wf)
+    @assert size(k, 1) == size(k, 2)
+    K = size(k, 1)
+    kern = Kernel{kernel_axes(k)}(wf)
     b1 = similar(a, size(kern, a))
     @btime map!($kern, $b1, $a)
+    b1
+end
 
-
-    # LoopVectorization.jl
+function bench_linfilt(k, a, ::Val{:LoopVectorization})
     print(rpad("  LoopVectorization", pd))
 
-    b5 = similar(a, axes(kern, a))
-    k5 = reshape(k, axes(kern))
-    @btime filter2davx!($b5, $a, $k5)
+    # from discourse forum:
+    # https://discourse.julialang.org/t/ann-statickernels-jl-fast-kernel-operations-on-arrays/37658
+    function filter2davx!(out::AbstractMatrix, A::AbstractMatrix, kern)
+        @avx for J in CartesianIndices(out)
+            tmp = zero(eltype(out))
+            for I ∈ CartesianIndices(kern)
+                tmp += A[I + J] * kern[I]
+            end
+            out[J] = tmp
+        end
+        out
+    end
+
+    b5 = similar(a, inner_axes(k, a))
+    k5 = reshape(k, kernel_axes(k))
+    @btime $filter2davx!($b5, $a, $k5)
+    #filter2davx!(b5, a, k5)
     # weird OffsetArray broadcast problem
     b5 = parent(b5)
+end
 
-
-    # NNlib.jl
+function bench_linfilt(k, a, ::Val{:NNlib})
     print(rpad("  NNlib", pd))
 
     ar = reshape(a, (size(a)..., 1, 1))
-    b2 = similar(a, (size(kern, a)..., 1, 1))
+    b2 = similar(a, (inner_size(k, a)..., 1, 1))
     kr = reshape(k, (size(k)..., 1, 1))
     cdims = DenseConvDims(ar, kr; stride=1, padding=0, dilation=1, flipkernel=true)
     @btime conv!($b2, $ar, $kr, $cdims)
+    b2
+end
 
-
-    # ImageFiltering.jl
+function bench_linfilt(k, a, ::Val{:ImageFiltering})
     print(rpad("  ImageFiltering", pd))
 
-    b3 = similar(a, axes(kern, a))
+    b3 = similar(a, inner_axes(k, a))
     kc = centered(k)
     @btime imfilter!($b3, $a, $kc, Inner())
     # weird OffsetArray broadcast problem
     b3 = parent(b3)
+end
 
-
-    # LocalFilters.jl
+function bench_linfilt(k, a, ::Val{:LocalFilters})
     print(rpad("  LocalFilters", pd))
 
     b4 = similar(a)
@@ -83,8 +86,22 @@ for N in (10, 100, 1000), K in (3, 5, 7)
     kr = LocalFilters.Kernel(reverse(reverse(k, dims=1), dims=2))
     @btime convolve!($b4, $a, $kr)
     # no way to only compute inner part
-    b4 = b4[axes(kern, a)...]
+    b4 = b4[inner_axes(k, a)...]
+end
 
 
-    @test b1 ≈ b2 ≈ b3 ≈ b4 ≈ b5
+# Linear Filtering
+
+for N in (100, 1000), K in (3, 5, 7)
+    a = rand(N, N)
+    k = rand(K, K)
+
+    println("Array: $(size(a)), Kernel: $(size(k))")
+
+    b = bench_linfilt(k, a, Val(:StaticKernels))
+
+    for s in [:LoopVectorization, :NNlib, :ImageFiltering, :LocalFilters]
+    #for s in [:LoopVectorization]
+        @test b ≈ bench_linfilt(k, a, Val(s))
+    end
 end
